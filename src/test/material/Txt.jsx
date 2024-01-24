9a0ecdf1a13c648a5f24cf4a13166b2402ce440b
Moemen Zaafarany <moemenzaafarany@gmail.com>
2024-01-24 13:39:14 +0200

    1

--- src/test/material/Txt.jsx
+++ src/test/material/Txt.jsx
@@ -2,21 +2,28 @@
 import React from "react";
 import { Typography } from "@mui/material";
 
-const T = {
-  p: (props) => <Typography {...props} />,
-  h1: (props) => <Typography variant="h1" {...props} />,
-  h2: (props) => <Typography variant="h2" {...props} />,
-  h3: (props) => <Typography variant="h3" {...props} />,
-  h4: (props) => <Typography variant="h4" {...props} />,
-  h5: (props) => <Typography variant="h5" {...props} />,
-  h6: (props) => <Typography variant="h6" {...props} />,
-  subtitle1: (props) => <Typography variant="subtitle1" {...props} />,
-  subtitle2: (props) => <Typography variant="subtitle2" {...props} />,
-  body1: (props) => <Typography variant="body1" {...props} />,
-  body2: (props) => <Typography variant="body2" {...props} />,
-  button: (props) => <Typography variant="button" {...props} />,
-  caption: (props) => <Typography variant="caption" {...props} />,
-  overline: (props) => <Typography variant="overline" {...props} />,
-  inherit: (props) => <Typography variant="inherit" {...props} />,
-};
-export default T;
+export const Txt = (props) => <Typography {...props} />;
+export const TxtH1 = (props) => <Typography variant="h1" {...props} />;
+export const TxtH2 = (props) => <Typography variant="h2" {...props} />;
+export const TxtH3 = (props) => <Typography variant="h3" {...props} />;
+export const TxtH4 = (props) => <Typography variant="h4" {...props} />;
+export const TxtH5 = (props) => <Typography variant="h5" {...props} />;
+export const TxtH6 = (props) => <Typography variant="h6" {...props} />;
+export const TxtSubtitle1 = (props) => (
+  <Typography variant="subtitle1" {...props} />
+);
+export const TxtSubtitle2 = (props) => (
+  <Typography variant="subtitle2" {...props} />
+);
+export const TxtBody1 = (props) => <Typography variant="body1" {...props} />;
+export const TxtBody2 = (props) => <Typography variant="body2" {...props} />;
+export const TxtButton = (props) => <Typography variant="button" {...props} />;
+export const TxtCaption = (props) => (
+  <Typography variant="caption" {...props} />
+);
+export const TxtOverline = (props) => (
+  <Typography variant="overline" {...props} />
+);
+export const TxtInherit = (props) => (
+  <Typography variant="inherit" {...props} />
+);
